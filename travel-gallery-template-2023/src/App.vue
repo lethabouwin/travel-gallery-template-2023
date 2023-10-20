ad61a0fe0948dbf9f52be565fc578dcb14244a17
Lethabo Mathabatha <lethabo@uwiniwin.co.za>
2023-10-20 12:40:17 +0200

    move code to WorldView page

--- travel-gallery-template-2023/src/App.vue
+++ travel-gallery-template-2023/src/App.vue
@@ -3,9 +3,12 @@ import HelloWorld from './components/HelloWorld.vue'
 import './App.css'
 
 import '../src/Pages/WorldView.vue'
-import WorldView from '../src/Pages/WorldView.vue';
+// import WorldView from '../src/Pages/WorldView.vue';
+import '../src/Pages/DestinationView.vue';
+import DestinationView from '../src/Pages/DestinationView.vue';
 </script>
 
 <template>
-  <WorldView />
+  <!-- <WorldView /> -->
+  <DestinationView />
 </template>
