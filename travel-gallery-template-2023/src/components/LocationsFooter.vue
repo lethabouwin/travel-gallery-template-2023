0edfb2e55d2fdf782e8e9755a11cdee6eab6f31b
Lethabo Mathabatha <lethabo@uwiniwin.co.za>
2023-10-20 11:05:36 +0200

    move 'locations container' to component

---
+++ travel-gallery-template-2023/src/components/LocationsFooter.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="locations-container">
+      <div class="location">
+        <div class="location-circle-marker-1">
+        </div>
+
+        <span class="location-name">Alaska, USA</span>
+        <span class="location-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem maxime quibusdam blanditiis.</span>
+      </div>
+
+      <div class="location">
+        <div class="location-circle-marker-2">
+        </div>
+
+        <span class="location-name">Sao Paulo, Brazil</span>
+        <span class="location-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem maxime quibusdam blanditiis.</span>
+      </div>
+
+      <div class="location">
+        <div class="location-circle-marker-3">
+        </div>
+
+        <span class="location-name">Lagos, Nigeria</span>
+        <span class="location-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem maxime quibusdam blanditiis.</span>
+      </div>
+
+      <div class="location">
+        <div class="location-circle-marker-4">
+        </div>
+
+        <span class="location-name">St Petersburg, Russia</span>
+        <span class="location-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem maxime quibusdam blanditiis.</span>
+      </div>
+
+      <div class="location">
+        <div class="location-circle-marker-5">
+        </div>
+
+        <span class="location-name">Sydney, Australia</span>
+        <span class="location-description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem maxime quibusdam blanditiis.</span>
+      </div>
+      
+    </div>
+  </div>
+</template>
