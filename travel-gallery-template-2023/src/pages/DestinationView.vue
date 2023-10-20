41740ec997d004613113b3b59f441608234bbce9
Lethabo Mathabatha <lethabo@uwiniwin.co.za>
2023-10-20 11:17:32 +0200

    add DestinationView page

---
+++ travel-gallery-template-2023/src/pages/DestinationView.vue
@@ -0,0 +1,20 @@
+<script setup>
+
+import '../App.css'
+import LocationsFooter from '../components/LocationsFooter.vue';
+</script>
+
+<template>
+  <div>
+    <div class="location-pins-container"></div>
+
+    <LocationsFooter />
+  </div>
+
+  <!-- pass a prop -->
+  <!-- <HelloWorld msg="" /> -->
+</template>
+
+<style scoped>
+ 
+</style>
