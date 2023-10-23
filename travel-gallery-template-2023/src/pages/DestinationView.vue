84d71e498d92609324730bdbcb4e947f6e91b8d1
Lethabo Mathabatha <lethabo@uwiniwin.co.za>
2023-10-23 10:46:43 +0200

    remove unused div tags

--- travel-gallery-template-2023/src/pages/DestinationView.vue
+++ travel-gallery-template-2023/src/pages/DestinationView.vue
@@ -10,21 +10,12 @@ import LocationsFooter from '../components/LocationsFooter.vue';
         <img src="../assets/chevron_left.svg" class="left-arrow" alt="">
 
         <div class="destination-images-container">
-            <div class="destination-image-1">
-                <img src="../assets/Pexels Photo by Albert Koenig.svg" class="destination-image-1" alt="pexels image" >
-            </div>
+            <img src="../assets/Pexels Photo by Albert Koenig.svg" class="destination-image-1" alt="pexels image" >
+            <img src="../assets/Pexels Photo by Barry Tan.svg" class="destination-image-2" alt="pexels image">
+            <img src="../assets/Pexels Photo by Gerhard Lipold.svg" class="destination-image-3" alt="pexels image">
+        </div>
 
-            <div class="destination-image-2">
-                <img src="../assets/Pexels Photo by Barry Tan.svg" class="destination-image-2" alt="pexels image">
-            </div>
-
-            <div class="destination-image-3">
-                <img src="../assets/Pexels Photo by Gerhard Lipold.svg" class="destination-image-3" alt="pexels image">
-            </div>
-            
-            </div>
-
-            <img src="../assets/chevron_right.svg" class="right-arrow" alt="">
+        <img src="../assets/chevron_right.svg" class="right-arrow" alt="">
         </div>
 
         <LocationsFooter />
