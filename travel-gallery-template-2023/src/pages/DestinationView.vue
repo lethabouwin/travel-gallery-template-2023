e3d96d0a21d65a143cbb56f9f97570ce56b08e0b
Lethabo Mathabatha <lethabo@uwiniwin.co.za>
2023-10-23 11:35:30 +0200

    ADD div tags

--- travel-gallery-template-2023/src/pages/DestinationView.vue
+++ travel-gallery-template-2023/src/pages/DestinationView.vue
@@ -6,16 +6,25 @@ import LocationsFooter from '../components/LocationsFooter.vue';
 
 <template>
     <div class="main-container">
+        <!-- carousel -->
         <div class="destination-container">
-        <img src="../assets/chevron_left.svg" class="left-arrow" alt="">
+            <img src="../assets/chevron_left.svg" class="prev-arrow" alt="">
 
-        <div class="destination-images-container">
-            <img src="../assets/Pexels Photo by Albert Koenig.svg" class="destination-image-1" alt="pexels image" >
-            <img src="../assets/Pexels Photo by Barry Tan.svg" class="destination-image-2" alt="pexels image">
-            <img src="../assets/Pexels Photo by Gerhard Lipold.svg" class="destination-image-3" alt="pexels image">
-        </div>
+            <div class="destination-images-container">
+                <div class="destination-slide">
+                    <img src="../assets/Pexels Photo by Albert Koenig.svg" class="destination-image-1" alt="pexels image" >
+                </div>
 
-        <img src="../assets/chevron_right.svg" class="right-arrow" alt="">
+                <div class="destination-slide">
+                    <img src="../assets/Pexels Photo by Barry Tan.svg" class="destination-image-2" alt="pexels image">
+                </div>
+
+                <div class="destination-slide">
+                    <img src="../assets/Pexels Photo by Gerhard Lipold.svg" class="destination-image-3" alt="pexels image">
+                </div>
+            </div>
+
+            <img src="../assets/chevron_right.svg" class="next-arrow" alt="">
         </div>
 
         <LocationsFooter />
