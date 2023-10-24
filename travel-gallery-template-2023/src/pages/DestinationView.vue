a2ed1156209966a4162f3e7b794f6d7451448db8
Lethabo Mathabatha <lethabo@uwiniwin.co.za>
2023-10-24 14:50:14 +0200

    add fullscreen styling

--- travel-gallery-template-2023/src/pages/DestinationView.vue
+++ travel-gallery-template-2023/src/pages/DestinationView.vue
@@ -1,8 +1,19 @@
 <script setup>
 import '../App.css'
 import LocationsFooter from '../components/LocationsFooter.vue';
-// import '../pages/DestinationView.css'
-// import sliderControls from '../pages/sliderControls.js';
+
+import image1 from '../assets/pexels-usa-1.svg';
+import image2 from '../assets/pexels-usa-2.svg';
+import image3 from '../assets/pexels-usa-3.svg';
+import '../assets/pexels-usa-1.svg';
+
+function fullView(imageSrc) {
+    // window.open(imageSrc, '_blank');
+    console.log('clicked')
+    document.getElementById('fullImage').src = imageSrc;
+    document.getElementById('fullImageView').style.display = 'block';
+}
+
 </script>
 
 <template>
@@ -14,27 +25,23 @@ import LocationsFooter from '../components/LocationsFooter.vue';
                 <input type="radio" name="slide" id="destination-image-2">
                 <input type="radio" name="slide" id="destination-image-3">
 
-                <!-- images need to be accepted as props -->
-                <img src="../assets/Pexels Photo by Albert Koenig.svg" class="destination-image-1" alt="pexels image" >
-                <img src="../assets/Pexels Photo by Barry Tan.svg" class="destination-image-2" alt="pexels image">
-                <img src="../assets/Pexels Photo by Gerhard Lipold.svg" class="destination-image-3" alt="pexels image">    
+                <img :src="image1" src="../assets/pexels-usa-1.svg" class="destination-image-1" alt="pexels image" @click="fullView('../assets/pexels-usa-1.svg')" >
+                <img :src="image2" src="../assets/pexels-usa-2.svg" class="destination-image-2" alt="pexels image" @click="fullView('../assets/pexels-usa-2.svg')">
+                <img :src="image3" src="../assets/pexels-usa-3.svg" class="destination-image-3" alt="pexels image" @click="fullView('../assets/pexels-usa-3.svg')">    
             </div>
-
-
-            </div>
+        </div>
 
             <div class="dots">
                 <label for="destination-image-1"></label>
                 <label for="destination-image-2"></label>
                 <label for="destination-image-3"></label>
             </div>
+
+            <div id="fullImageView" style="display: none;">
+                <img id="fullImage">
+            </div>
+
         <LocationsFooter />
     </div>
 
-  <!-- pass a prop -->
-  <!-- <HelloWorld msg="" /> -->
 </template>
-
-<style scoped>
- 
-</style>
