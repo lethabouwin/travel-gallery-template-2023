9d220b69d910fcceb735b99ba515fe4cf9061d09
Lethabo Mathabatha <lethabo@uwiniwin.co.za>
2023-10-30 11:55:27 +0200

    add image path components

---
+++ travel-gallery-template-2023/src/components/imagesDublin.js
@@ -0,0 +1,6 @@
+// images for location-2
+export const imagesDublin = [
+    "../assets/imagesDublinSanlam/EliteDinner1.jpg",
+    "../assets/imagesDublinSanlam/EliteDinner2.JPG",
+    "../assets/imagesDublinSanlam/EliteDinner3.JPG"
+  ]
